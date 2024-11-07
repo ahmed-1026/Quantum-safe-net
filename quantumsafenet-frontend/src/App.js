3a819de8f343f1cef98e319467b1c200d2d06565
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-11-07 06:53:02 +0500

    add basic dashboard

--- quantumsafenet-frontend/src/App.js
+++ quantumsafenet-frontend/src/App.js
@@ -1,7 +1,29 @@
-export default function App() {
+import React from "react";
+// import Navbar from './components/navbar';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import Dashboard from "./components/dashboard";
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <div className="">
+        <QuantumSafeNet />
+      </div>
+    </Router>
+  );
 }
+
+function QuantumSafeNet() {
+  return (
+    <>
+      <div>
+        <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
